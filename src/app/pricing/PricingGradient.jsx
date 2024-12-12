4ace9ce2e50b317f4e45d56a40c5fe213c2706b4
Kiran jadhav <Kiran.Jadhav@xbiztechventuresoutlook.onmicrosoft.com>
2024-12-13 00:13:10 +0530

    fixed build issue

--- src/app/pricing/PricingGradient.jsx
+++ src/app/pricing/PricingGradient.jsx
@@ -12,7 +12,7 @@ const PricingGradient = () => {
             <div style={{ background: 'linear-gradient(90.00deg, rgb(255, 0, 115),rgb(132, 0, 255) 100%)' }} className=' md:py-[88px] py-[127px] md:px-[40px] relative px-6'>
                 <div className='relative z-10'>
                     <h2 className='font-afacad text-center font-bold  lg:text-8xl md:text-[56px] md:leading-[74px] text-5xl '>Ready to bring your ideas to life? Schedule a call with us today!</h2>
-                    <p className='text-center max-w-[600px] mx-auto md:mt-10 mt-5'> Our team is here to help you succeed. Let's work together to achieve your goals and elevate your brand to new heights.</p>
+                    <p className='text-center max-w-[600px] mx-auto md:mt-10 mt-5'> Our team is here to help you succeed. Let&apos;s work together to achieve your goals and elevate your brand to new heights.</p>
                     <div className='flex justify-center'>
                         <button onClick={()=>setShowModal(true)} className='flex items-center gap-2  bg-brand text-white cursor-pointer transition-all duration-300 w-fit px-6 py-2 md:px-6 md:py-[12px] rounded-[30px] lg:mt-14 md:mt-10 mt-6 relative'>
                             <span className='text-base font-bold'>Book Call</span>
