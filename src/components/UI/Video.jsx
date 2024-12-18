94faf7258dcdb2042e485b40c59dd18a76eeab0b
Kiran jadhav <Kiran.Jadhav@xbiztechventuresoutlook.onmicrosoft.com>
2024-12-18 20:43:49 +0530

    video updated

--- src/components/UI/Video.jsx
+++ src/components/UI/Video.jsx
@@ -3,7 +3,7 @@ const Video = () => {
     return (
         <div>
             <video autoPlay loop muted className='w-full lg:h-[700px] md:h-[362px] h-[218px] object-cover'>
-                <source src="https://buildit-product.s3.ap-south-1.amazonaws.com/gunpowder+innovation.mp4" type="video/mp4" />
+                <source src="https://gunpowderinnovations.s3.eu-west-2.amazonaws.com/videos/gunpowderinnovations.mp4" type="video/mp4" />
             </video>
         </div>
     )
