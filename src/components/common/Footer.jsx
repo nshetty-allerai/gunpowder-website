2815ca5b47305df465bd9dcd84b2b853919bf2f2
Kiran jadhav <Kiran.Jadhav@xbiztechventuresoutlook.onmicrosoft.com>
2025-01-14 23:26:29 +0530

    adjust margin

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -121,7 +121,7 @@ const Footer = () => {
                     </Link>
 
                 </div>
-                <div className='border-t border-t-[#EAECF0] mt-20 mb-12 '></div>
+                <div className='border-t border-t-[#EAECF0] mt-8 mb-12 '></div>
                 <div className='flex-col 2xl:flex-row 2xl:gap-4 gap-8 flex items-center justify-between'>
                     <div className='text-center'>© 2024 Gunpowder Innovations (a trading name of AllerAI Limited). All rights reserved.</div>
                     <div className='flex flex-wrap shrink-0 gap-4 justify-center items-center mt-6 md:mt-0'>
